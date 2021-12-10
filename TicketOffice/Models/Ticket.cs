2457946799856b6469ac11eed35536c9628d2e5f
Maxim <fleshik2@gmail.com>
2021-12-10 13:41:07 +0300

    update

---
+++ TicketOffice/Models/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketOffice.Models
+{
+    public class Ticket
+    {
+        private string _pointOfDeparture;
+        private string _pointOfDestination;
+        private DateTime _date;
+        private string _classType;
+
+        public Ticket(string pointOfDeparture, string pointOfDestination, DateTime date, string classType)
+        {
+            PointOfDeparture = pointOfDeparture;
+            PointOfDestination = pointOfDestination;
+            Date = date;
+            ClassType = classType;
+        }
+
+        public string PointOfDeparture { get => _pointOfDeparture; set => _pointOfDeparture = value; }
+        public string PointOfDestination { get => _pointOfDestination; set => _pointOfDestination = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+        public string ClassType { get => _classType; set => _classType = value; }
+    }
+}
