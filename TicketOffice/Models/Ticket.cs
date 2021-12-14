c0c2195a0adf001d20b39139673fadb62f04b6c1
Maxim <fleshik2@gmail.com>
2021-12-14 12:51:51 +0300

    new components added

--- TicketOffice/Models/Ticket.cs
+++ TicketOffice/Models/Ticket.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace TicketOffice.Models
 {
     public class Ticket
     {
+        
+
         private string _pointOfDeparture;
         private string _pointOfDestination;
         private DateTime _date;
@@ -20,6 +25,16 @@ namespace TicketOffice.Models
             ClassType = classType;
         }
 
+        public Ticket(string pointOfDeparture, string pointOfDestination, string classType)
+        {
+            PointOfDeparture = pointOfDeparture;
+            PointOfDestination = pointOfDestination;
+            ClassType = classType;
+        }
+
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
         public string PointOfDeparture { get => _pointOfDeparture; set => _pointOfDeparture = value; }
         public string PointOfDestination { get => _pointOfDestination; set => _pointOfDestination = value; }
         public DateTime Date { get => _date; set => _date = value; }
